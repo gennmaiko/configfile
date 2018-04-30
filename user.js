a05eac17f99b7602cfbbaa4eeeb10c22b0ed6e26
げんまいこ <gennmaiko@gmail.com>
2018-04-30 17:12:26 +0900

    Add files via upload

---
+++ user.js
@@ -0,0 +1,16 @@
+user_pref("browser.search.openintab", true);
+user_pref("dom.event.contextmenu.enabled", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.animate", false);
+user_pref("browser.cache.memory.capacity", 233016);
+user_pref("gfx.canvas.azure.accelerated", true);
+user_pref("layers.acceleration.force-enabled", true);
+user_pref("layers.omtp.enabled", true);
+user_pref("mousewheel.default.delta_multiplier_x", 600);
+user_pref("mousewheel.default.delta_multiplier_y", 350);
+user_pref("mousewheel.min_line_scroll_amount", 15);
+user_pref("mousewheel.with_shift.delta_multiplier_y", 600);
+user_pref("image.mem.max_decoded_image_kb", 512000);
+user_pref("image.mem.min_discard_timeout_ms", 2100000000);
+user_pref("nglayout.initialpaint.delay", 0);
