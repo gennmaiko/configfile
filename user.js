44a03d1a286ba649b73a78dcbbd125b93f9d8ba2
げんまいこ <gennmaiko@gmail.com>
2018-06-19 11:59:47 +0900

    Update user.js

--- user.js
+++ user.js
@@ -1,9 +1,10 @@
-user_pref("browser.search.openintab", true);
-user_pref("dom.event.contextmenu.enabled", false);
-user_pref("browser.tabs.closeWindowWithLastTab", false);
-user_pref("browser.tabs.warnOnClose", false);
-user_pref("browser.tabs.animate", false);
 user_pref("browser.cache.memory.capacity", 233016);
+user_pref("browser.search.openintab", true);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.animate", false);
+user_pref("browser.tabs.loadBookmarksInTabs", true);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("dom.event.contextmenu.enabled", false);
 user_pref("gfx.canvas.azure.accelerated", true);
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("layers.omtp.enabled", true);
@@ -14,4 +15,4 @@ user_pref("mousewheel.with_shift.delta_multiplier_y", 600);
 user_pref("image.mem.max_decoded_image_kb", 512000);
 user_pref("image.mem.min_discard_timeout_ms", 2100000000);
 user_pref("nglayout.initialpaint.delay", 0);
-user_pref("browser.tabs.loadBookmarksInTabs", true);
+
